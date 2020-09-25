5c21ea133cb3ce21b8f57e8012ac80b1970ada33
Hellebore <nthapen@gmail.com>
2020-09-25 07:50:53 +0100

    Metrics by default (#22)

--- src/extension.ts
+++ src/extension.ts
@@ -27,7 +27,7 @@ function startLangServer(args: string[], documentSelector: string[], context: Ex
     const clientOptions: LanguageClientOptions = {
         documentSelector: documentSelector,
         synchronize: {
-            configurationSection: "sourcery.token"
+            configurationSection: "sourcery"
         },
         initializationOptions: {
             'token': token,
@@ -38,7 +38,7 @@ function startLangServer(args: string[], documentSelector: string[], context: Ex
 
     if (!token) {
         const readmePath = Uri.file(
-            path.join(context.extensionPath, "INSTALL.md")
+            path.join(context.extensionPath, "INSTALL.py")
         );
         window.showTextDocument(readmePath);
         const result = window.showInputBox({
