6e7b01088dbd0e1f5d610bd83ec58960e3f0cede
Ben <ben@sourcery.ai>
2022-10-21 13:42:56 +0100

    refactor: move hub container markup to a separate file

--- src/extension.ts
+++ src/extension.ts
@@ -24,6 +24,7 @@ import {
     ExecuteCommandRequest,
     ExecuteCommandParams
 } from 'vscode-languageclient';
+import {readFileSync} from "fs";
 
 
 function createLangServer(context: ExtensionContext): LanguageClient {
@@ -161,41 +162,7 @@ export function activate(context: ExtensionContext) {
             enableScripts: true,
           }
       );
-      panel.webview.html = `<!DOCTYPE html>
-        <head>
-          <style>
-          body,
-          html {
-            width: 100%;
-            height: 100%;
-            margin: 0;
-            padding: 0;
-          }
-          .container {
-            display: flex;
-            width: 100%;
-            height: 100%;
-            flex-direction: column;
-            overflow: hidden;
-          }
-          #theFrame {
-            flex-grow: 1;
-            border: none;
-            margin: 0;
-            padding: 0;
-          }
-        </style>
-      </head>
-      <html lang="en" style="height: 100%; width: 100%; margin: 0; padding: 0">
-        <body style="height: 100%; width: 100%; margin: 0; padding: 0">
-          <div class="container">
-            <iframe
-              id="theFrame"
-              src="http://127.0.0.1:61816/home"
-            ></iframe>
-          </div>
-        </body>
-      </html>`;
+      panel.webview.html = readFileSync(path.join(context.extensionPath, 'assets', 'hubContainer.html')).toString();
     })
     );
 
