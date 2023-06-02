986dd53997dbc03136595a7a03f6a059971c2a08
Hellebore <nthapen@gmail.com>
2023-06-02 14:11:39 +0100

    format js and css files

--- media/chat.js
+++ media/chat.js
@@ -3,64 +3,60 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
 (function () {
+  const vscode = acquireVsCodeApi();
+  const chatContainer = document.querySelector(".chatContainer");
+  const messageInput = document.querySelector(".message-input");
+  let currentAssistantMessage;
 
-    const vscode = acquireVsCodeApi();
-    const chatContainer = document.querySelector('.chatContainer');
-    const messageInput = document.querySelector('.message-input');
-    let currentAssistantMessage;
+  // Function to add a user message to the chat interface
+  function addUserMessage(message) {
+    let userMessageElement = document.createElement("div");
+    userMessageElement.classList.add("user-message");
+    userMessageElement.textContent = message;
+    chatContainer.append(userMessageElement);
+    currentAssistantMessage = null;
+  }
 
-    // Function to add a user message to the chat interface
-    function addUserMessage(message) {
-        let userMessageElement = document.createElement('div');
-        userMessageElement.classList.add('user-message');
-        userMessageElement.textContent = message;
-        chatContainer.append(userMessageElement);
-        currentAssistantMessage = null;
+  // Function to add an assistant message to the chat interface
+  function addAssistantMessage(message) {
+    if (currentAssistantMessage != null) {
+      currentAssistantMessage.textContent =
+        currentAssistantMessage.textContent + message;
+    } else {
+      let assistantMessageElement = document.createElement("div");
+      assistantMessageElement.classList.add("assistant-message");
+      assistantMessageElement.textContent = message;
+      chatContainer.append(assistantMessageElement);
+      currentAssistantMessage = assistantMessageElement;
     }
+  }
 
-    // Function to add an assistant message to the chat interface
-    function addAssistantMessage(message) {
-        if (currentAssistantMessage != null) {
-            currentAssistantMessage.textContent = currentAssistantMessage.textContent + message;
-        } else {
-            let assistantMessageElement = document.createElement('div');
-            assistantMessageElement.classList.add('assistant-message');
-            assistantMessageElement.textContent = message;
-            chatContainer.append(assistantMessageElement);
-            currentAssistantMessage = assistantMessageElement;
-        }
+  function clearMessages() {
+    currentAssistantMessage = null;
+    chatContainer.textContent = "";
+  }
 
+  messageInput.addEventListener("keypress", (e) => {
+    if (e.which === 13) {
+      e.preventDefault();
+      let message = messageInput.value.trim();
+      messageInput.value = "";
+      addUserMessage(message);
+      sendMessageToExtension(message);
     }
+  });
 
-    function clearMessages() {
-        currentAssistantMessage = null;
-        chatContainer.textContent = "";
+  window.addEventListener("message", (event) => {
+    const message = event.data;
+
+    if (message.command === "add_result") {
+      addAssistantMessage(message.result);
+    } else if (message.command === "clear_chat") {
+      clearMessages();
     }
+  });
 
-    messageInput.addEventListener('keypress', (e) => {
-        if (e.which === 13) {
-            e.preventDefault();
-            let message = messageInput.value.trim();
-            messageInput.value = "";
-            addUserMessage(message);
-            sendMessageToExtension(message);
-
-        }
-    });
-
-    window.addEventListener('message', event => {
-      const message = event.data;
-
-      if (message.command === 'add_result') {
-          addAssistantMessage(message.result);
-      } else if (message.command === "clear_chat") {
-          clearMessages();
-        }
-    });
-
-
-    function sendMessageToExtension(message) {
-        vscode.postMessage({ type: "chat_request", data: message});
-    }
-
-}());
+  function sendMessageToExtension(message) {
+    vscode.postMessage({ type: "chat_request", data: message });
+  }
+})();
