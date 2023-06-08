f091ba9a4568ed6a9c7508a66b1e934f87ff21b7
Nick Thapen <nthapen@gmail.com>
2023-06-08 10:56:55 +0100

    feat: improve UX of chat interface (#156)

--- src/chat.ts
+++ src/chat.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
 import { randomBytes } from "crypto";
-import { getValidInput } from "./extension";
 
 export class ChatProvider implements vscode.WebviewViewProvider {
   public static readonly viewType = "sourcery.chat";
@@ -31,7 +30,11 @@ export class ChatProvider implements vscode.WebviewViewProvider {
       webviewView.webview
     );
 
-    const input = getValidInput();
+    webviewView.onDidChangeVisibility(() => {
+      if (this._view.visible) {
+        this._view.webview.postMessage({ command: "focus" });
+      }
+    });
 
     webviewView.webview.onDidReceiveMessage(async (data) => {
       switch (data.type) {
@@ -67,6 +70,9 @@ export class ChatProvider implements vscode.WebviewViewProvider {
     const styleMainUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "media", "chat.css")
     );
+    const animationsUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "animations.css")
+    );
     // Use a nonce to only allow a specific script to be run.
     const nonce = randomBytes(16).toString("base64");
 
@@ -97,13 +103,29 @@ export class ChatProvider implements vscode.WebviewViewProvider {
 				<link href="${styleResetUri}" rel="stylesheet">
 				<link href="${styleVSCodeUri}" rel="stylesheet">
 				<link href="${styleMainUri}" rel="stylesheet">
+				<link href="${animationsUri}" rel="stylesheet">
 			</head>
 			<body>
-				<div class="chatContainer">
-				</div>
-				<div class="inputContainer">
-					<input type="text" class="message-input" placeholder="Type your message..." autofocus>
-				</div>
+                <section class="sidebar__section-container active" data-section="chat-assistant">
+                  <ul class="sidebar__chat-assistant--dialogue-container">
+  
+                  </ul>
+                  <footer class="sidebar__chat-assistant--footer">
+                    <section class="sidebar__chat-assistant--textarea-container">
+                      <textarea class="sidebar__chat-assistant--textarea" placeholder="Type your message here!"
+                        id="user-prompt"></textarea>
+                      <button class="sidebar__chat-assistant--textarea-send-button sidebar__textarea-send-button--disabled"
+                        id="send-button">
+                        <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"
+                          class="sidebar__chat-assistant--textarea-send-icon">
+                          <path
+                            d="m498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6l-119.6-49.7-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1s-20.3-16.7-20.3-29.9v-83.6c0-4 1.5-7.8 4.2-10.7l167.6-182.9c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7l-203.4 180.7-88.3-44.2c-10.6-5.3-17.4-15.9-17.7-27.7s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" />
+                        </svg>
+                      </button>
+                    </section>
+                  </footer>
+                </section>
+
 			</body>
 			<script nonce="${nonce}" src="${scriptUri}"></script>
 			</html>`;
--- src/webview/chat.js
+++ src/webview/chat.js
@@ -4,58 +4,126 @@
 // It cannot access the main VS Code APIs directly.
 (function () {
   const vscode = acquireVsCodeApi();
-  const chatContainer = document.querySelector(".chatContainer");
-  const messageInput = document.querySelector(".message-input");
+
+  const chatContainer = document.querySelector(
+    ".sidebar__chat-assistant--dialogue-container"
+  );
+  const messageInput = document.getElementById("user-prompt");
+  messageInput.focus();
+
+  const sendButton = document.getElementById("send-button");
+  sendButton.onclick = sendUserMessage;
+
+  // Hold the current assistant message so we can direct streaming responses to it
   let currentAssistantMessage;
 
+  // Communication between the webview and the extension proper
+  window.addEventListener("message", (event) => {
+    const message = event.data;
+
+    if (message.command === "add_result") {
+      addAssistantMessageToUI(message.result);
+    } else if (message.command === "clear_chat") {
+      clearAllMessages();
+    } else if (message.command === "focus") {
+      messageInput.focus();
+    }
+  });
+  function sendMessageToExtension(message) {
+    vscode.postMessage({ type: "chat_request", data: message });
+  }
+
+  function clearAllMessages() {
+    currentAssistantMessage = null;
+    chatContainer.textContent = "";
+  }
+
+  function sendUserMessage() {
+    const message = messageInput.value.trim();
+    messageInput.value = "";
+    addUserMessageToUI(message);
+    sendMessageToExtension(message);
+    checkTextarea();
+  }
+
   // Function to add a user message to the chat interface
-  function addUserMessage(message) {
-    let userMessageElement = document.createElement("div");
-    userMessageElement.classList.add("user-message");
-    userMessageElement.textContent = message;
+  function addUserMessageToUI(message) {
+    const templateMessage = `
+            <div class="sidebar__chat-assistant--chat-bubble-content-user">
+              <p class="sidebar__chat-assistant--chat-bubble-text">
+                ${message}
+              </p>
+            </div>
+            <div class="sidebar__chat-assistant--chat-avatar-container">
+              <span class="sidebar__chat-assistant--agent-avatar-image">🧙🏻‍♂️</span>
+            </div>
+    `;
+    const userMessageElement = document.createElement("li");
+    userMessageElement.classList.add("sidebar__chat-assistant--chat-bubble");
+    userMessageElement.classList.add(
+      "sidebar__chat-assistant--chat-bubble-user"
+    );
+    userMessageElement.innerHTML = templateMessage;
     chatContainer.append(userMessageElement);
     currentAssistantMessage = null;
   }
 
   // Function to add an assistant message or add to the existing one
-  function addAssistantMessage(message) {
+  function addAssistantMessageToUI(message) {
     if (currentAssistantMessage != null) {
       currentAssistantMessage.textContent += message;
     } else {
-      let assistantMessageElement = document.createElement("div");
-      assistantMessageElement.classList.add("assistant-message");
-      assistantMessageElement.textContent = message;
+      const templateContents = `
+            <!-- Using an absolute sourcery.ai URL for now, since I'm not sure how does VS Code extensions handle static assets. -->
+            <div class="sidebar__chat-assistant--chat-avatar-container">
+              <img src="https://sourcery.ai/favicon-32x32.png?v=63c3364394c84cae06d42bc320066118" alt="Sourcery logo"
+                class="sidebar__chat-assistant--agent-avatar-image" />
+            </div>
+            <div class="sidebar__chat-assistant--chat-bubble-content-assistant">
+              <p class="sidebar__chat-assistant--chat-bubble-text">
+                ${message}
+              </p>
+            </div>`;
+
+      const assistantMessageElement = document.createElement("li");
+      assistantMessageElement.classList.add(
+        "sidebar__chat-assistant--chat-bubble"
+      );
+      assistantMessageElement.classList.add(
+        "sidebar__chat-assistant--chat-bubble-agent"
+      );
+      assistantMessageElement.innerHTML = templateContents;
       chatContainer.append(assistantMessageElement);
-      currentAssistantMessage = assistantMessageElement;
+      currentAssistantMessage = assistantMessageElement.querySelector(
+        ".sidebar__chat-assistant--chat-bubble-text"
+      );
     }
   }
 
-  function clearMessages() {
-    currentAssistantMessage = null;
-    chatContainer.textContent = "";
+  // Enable/Disable send button depending on whether text area is empty
+  function checkTextarea() {
+    if (messageInput.value.trim() !== "") {
+      sendButton.classList.remove("sidebar__textarea-send-button--disabled");
+    } else {
+      sendButton.classList.add("sidebar__textarea-send-button--disabled");
+    }
   }
 
+  // Check for disable/enable send button
+  messageInput.addEventListener("input", checkTextarea);
+
+  // Check to see if we need to disable send button on backspace
+  messageInput.addEventListener("keydown", function (event) {
+    if (event.key === "Backspace") {
+      setTimeout(checkTextarea, 0);
+    }
+  });
+
+  // Listen for return key in order to send user messages
   messageInput.addEventListener("keypress", (e) => {
     if (e.which === 13 && !e.shiftKey) {
       e.preventDefault();
-      let message = messageInput.value.trim();
-      messageInput.value = "";
-      addUserMessage(message);
-      sendMessageToExtension(message);
+      sendUserMessage();
     }
   });
-
-  window.addEventListener("message", (event) => {
-    const message = event.data;
-
-    if (message.command === "add_result") {
-      addAssistantMessage(message.result);
-    } else if (message.command === "clear_chat") {
-      clearMessages();
-    }
-  });
-
-  function sendMessageToExtension(message) {
-    vscode.postMessage({ type: "chat_request", data: message });
-  }
 })();
