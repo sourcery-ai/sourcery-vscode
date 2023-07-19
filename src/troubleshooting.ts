c0b05df59d252e5b307b8f970b0330d9f272c11d
Ben <ben@sourcery.ai>
2023-07-19 16:25:17 +0100

    feat: add troubleshooting window and connect to backend

---
+++ src/troubleshooting.ts
@@ -0,0 +1,76 @@
+import {
+  CancellationToken,
+  ExtensionContext,
+  Uri,
+  WebviewView,
+  WebviewViewProvider,
+  WebviewViewResolveContext,
+} from "vscode";
+import { randomBytes } from "crypto";
+import * as vscode from "vscode";
+
+interface Message {
+  action: string;
+}
+
+export class TroubleshootingProvider implements WebviewViewProvider {
+  public static readonly viewType = "sourcery.troubleshooting";
+  private _view?: WebviewView;
+  private _extensionUri: Uri;
+
+  constructor(private _context: ExtensionContext) {
+    this._extensionUri = _context.extensionUri;
+  }
+
+  public async resolveWebviewView(
+    webviewView: WebviewView,
+    context: WebviewViewResolveContext,
+    token: CancellationToken
+  ) {
+    this._view = webviewView;
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.onDidReceiveMessage(async (message: Message) => {
+      vscode.commands.executeCommand("sourcery.troubleshoot", message);
+    });
+
+    const styleSheets = [
+      "reset.css",
+      "vscode.css",
+      "chat.css",
+      "troubleshooting.css",
+    ]
+      .map((file) => Uri.joinPath(this._extensionUri, "media", file))
+      .map(webviewView.webview.asWebviewUri)
+      .map((webviewUri) => `<link href="${webviewUri}" rel="stylesheet">`)
+      .join("\n");
+
+    const scriptUri = webviewView.webview.asWebviewUri(
+      Uri.joinPath(this._extensionUri, "src", "webview", "troubleshooting.js")
+    );
+
+    const nonce = randomBytes(16).toString("base64");
+
+    const cspStr = `default-src 'none'; style-src ${webviewView.webview.cspSource} 'nonce-${nonce}'; script-src 'nonce-${nonce}'; img-src * 'self' https:;`;
+
+    webviewView.webview.html = `<!DOCTYPE html>
+<html lang="en" style="height: 100%">
+<head>
+    <title>Troubleshooting</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="${cspStr}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    ${styleSheets}
+</head>
+<body class="troubleshooting" id="main">
+
+</body>
+<script nonce="${nonce}" src="${scriptUri}"></script>
+</html>
+    `;
+  }
+}
---
+++ src/webview/troubleshooting.ts
@@ -0,0 +1,81 @@
+/**
+ * Script for the Troubleshooting webview.
+ *
+ * This TypeScript file should be compiled using `npm run compile-webviews` which will create an associated
+ * JavaScript file, which is what is actually included in the webview HTML.
+ * */
+
+declare const acquireVsCodeApi: any;
+
+type SubmitMessage = {
+  action: "submit";
+  promptValue: string;
+};
+
+type Message = SubmitMessage; // anticipating future Message types
+
+type PostMessage = (message: Message) => void;
+
+function createMessagePoster(vscode: any): PostMessage {
+  // Create a wrapper function which is strongly typed
+  return (message: Message) => vscode.postMessage(message);
+}
+
+function createElement<K extends keyof HTMLElementTagNameMap>(
+  tagName: K,
+  className: string,
+  children: HTMLElement[] = []
+): HTMLElementTagNameMap[K] {
+  // Generically create an element and attach its children
+  const element = document.createElement(tagName);
+  element.className = className;
+  children.forEach((child) => element.appendChild(child));
+  return element;
+}
+
+function createPrompt(): HTMLTextAreaElement {
+  // Create the prompt and add custom placeholder and ID
+  const prompt = createElement("textarea", "troubleshooting__prompt");
+  prompt.id = "prompt";
+  prompt.placeholder = "Describe the issue in detail.";
+  return prompt;
+}
+
+function getPrompt(): HTMLTextAreaElement {
+  // Return the prompt, if it exists (which it should)
+  return document.getElementById("prompt") as HTMLTextAreaElement;
+}
+
+function createSubmitButton(postMessage: PostMessage): HTMLButtonElement {
+  // Create the submit button and attach submit action
+  // Would this system be better as a form?
+  const submitButton = createElement("button", "troubleshooting__send_button");
+  submitButton.innerText = "Submit";
+  submitButton.onclick = () => {
+    postMessage({ action: "submit", promptValue: getPrompt().value });
+  };
+  return submitButton;
+}
+
+function getMain() {
+  return document.getElementById("main");
+}
+
+function init(postMessage: PostMessage) {
+  // Add children to main element
+  // It's easier to do this in the script because we can reload the webview in development, rather than restarting
+  // the whole extension development host.
+  getMain().append(
+    createElement("section", "troubleshooting__input", [
+      createPrompt(),
+      createSubmitButton(postMessage),
+    ]),
+    createElement("section", "troubleshooting__main")
+  );
+}
+
+(function () {
+  const vscode = acquireVsCodeApi();
+  const postMessage = createMessagePoster(vscode);
+  init(postMessage);
+})();
